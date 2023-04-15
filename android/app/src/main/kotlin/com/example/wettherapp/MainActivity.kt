0bd96a8bb46430f51d7a5535c8628b552cccc4ca
ghanbaridev <104554904+ghanbaridev@users.noreply.github.com>
2023-04-15 14:17:11 +0330

    added screns and servisec to the app

---
+++ android/app/src/main/kotlin/com/example/wettherapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wettherapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    getlocation();
+    super.initState();
+  }
+
+  void getlocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

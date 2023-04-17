142bc2cd270f6d67781cfcd5ca97d4852877502c
ghanbaridev <104554904+ghanbaridev@users.noreply.github.com>
2023-04-17 14:08:40 +0330

    modified code

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:wettherapp/services/location.dart';
+import 'package:wettherapp/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'package:wettherapp/services/networking.dart';
+
+const apikey = "7b114153676f091855ed68761b553c29";
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,16 +14,24 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double? latitude;
+  double? longitude;
   @override
   void initState() {
-    getlocation();
+    getlocationdata();
+
     super.initState();
   }
 
-  void getlocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  void getlocationdata() async {
+    location locat = location();
+    await locat.getcurrentlocation();
+    latitude = locat.latitude;
+    longitude = locat.longitde;
+    Netwoekhelper netwoekhelper = Netwoekhelper(
+        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apikey");
+
+    var wetherdata = await netwoekhelper.getdata();
   }
 
   @override
